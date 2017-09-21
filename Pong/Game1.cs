453575692175414d66a65ccb80064b43aff0b2b9
TimGoedhuys <32095184+TimGoedhuys@users.noreply.github.com>
2017-09-21 18:37:43 +0200

    we hebben nu een menu
    heeft alleen wat finetuning nodig qua lay-out en verdere functies

--- Pong/Game1.cs
+++ Pong/Game1.cs
@@ -9,6 +9,7 @@ namespace Pong
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
+    
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
@@ -21,7 +22,7 @@ namespace Pong
         Bar m_Bar2;
         Ball m_Ball;
         Song Music;
-        SoundEffectInstance soundEffectInstance;
+        Button PlayButton;
 
         public class Ball
         {            
@@ -61,6 +62,60 @@ namespace Pong
             public void SetPos(float NewPos) { m_Pos.Y = NewPos; }
             public void SetVel(float Vel) { m_Vel = Vel; }
             public float GetMaxVel() { return m_MaxVel; }
+        }
+
+        enum Gamestate
+        {
+            MainMenu,
+            Options,
+            Playing,
+        }
+
+        Gamestate CurrentGameState = Gamestate.MainMenu;
+
+        class Button
+        {
+            Texture2D Texture;
+            Vector2 Position;
+            Rectangle Rectangle;
+
+            Color Colour = new Color(255, 255, 255, 255);
+            public Vector2 Size;
+            public Button(Texture2D NewTexture ,GraphicsDevice graphics)
+            {
+                Texture = NewTexture;
+                Size = new Vector2( graphics.Viewport.Width/8 , graphics.Viewport.Height / 25);
+            }
+
+            bool Down;
+            public bool IsClicked;
+            public void Update(MouseState Mouse)
+            {
+                Rectangle = new Rectangle((int)Position.X , (int)Position.Y , (int)Size.X , (int)Size.Y);
+
+                Rectangle MouseRectangle = new Rectangle(Mouse.X, Mouse.Y, 1, 1);
+                if (MouseRectangle.Intersects(Rectangle))
+                {
+                    if (Colour.A == 10) Down = false;
+                    if (Colour.A == 0) Down = true;
+                    if (Down) Colour.A += 3; else Colour.A -= 3;
+                    if (Mouse.LeftButton == ButtonState.Pressed) IsClicked = true;  
+                }
+                else if(Colour.A < 255)
+                {
+                    Colour.A += 3;
+                    IsClicked = false;
+                } 
+            }
+            public void SetPostion(Vector2 NewPosition)
+            {
+                Position = NewPosition;
+            }
+            public void Draw(SpriteBatch spriteBatch)
+            {
+                spriteBatch.Draw(Texture, Rectangle, Colour);
+            }
+
         }
 
         public Game1()
@@ -114,6 +169,10 @@ namespace Pong
             Music = Content.Load<Song>("BeepBox-Song loop");
             MediaPlayer.Play(Music);
             MediaPlayer.IsRepeating = true;
+
+            IsMouseVisible = true;
+            PlayButton = new Button(Content.Load<Texture2D>("Play") , graphics.GraphicsDevice);
+            PlayButton.SetPostion(new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2 ));
         }
 
         /// <summary>
@@ -137,45 +196,55 @@ namespace Pong
 
             // TODO: Add your update logic here
             KeyboardState Keystate = Keyboard.GetState();
-
-            if (Keyboard.GetState().IsKeyDown(Keys.S)) { m_Bar1.SetVel(m_Bar1.GetMaxVel()); }
-            if (Keyboard.GetState().IsKeyDown(Keys.W)) { m_Bar1.SetVel(-m_Bar1.GetMaxVel()); }
-            if (Keystate.IsKeyDown(Keys.Down)) { m_Bar2.SetVel(m_Bar2.GetMaxVel()); }
-            if (Keystate.IsKeyDown(Keys.Up)) { m_Bar2.SetVel(-m_Bar2.GetMaxVel()); }
-            if (m_Bar1.GetPosY() <= 0 && m_Bar1.GetVel() < 0 || Keyboard.GetState().IsKeyUp(Keys.S) && m_Bar1.GetVel() > 0) { m_Bar1.SetVel(0); }
-            if (m_Bar1.GetPosY() + m_Bar2.GetHeight() >= graphics.GraphicsDevice.Viewport.Height && m_Bar1.GetVel() > 0 || Keyboard.GetState().IsKeyUp(Keys.W) && m_Bar1.GetVel() < 0) { m_Bar1.SetVel(0); }
-            if (m_Bar2.GetPosY() <= 0 && m_Bar2.GetVel() < 0 || Keyboard.GetState().IsKeyUp(Keys.Down) && m_Bar2.GetVel() > 0) { m_Bar2.SetVel(0); }
-            if (m_Bar2.GetPosY() + m_Bar2.GetHeight() >= graphics.GraphicsDevice.Viewport.Height && m_Bar2.GetVel() > 0 || Keyboard.GetState().IsKeyUp(Keys.Up) && m_Bar2.GetVel() < 0) { m_Bar2.SetVel(0); }
-
-            
-
-            if (m_Bar1.GetPosY() + m_Bar1.GetHeight() >= m_Ball.GetPosY() + m_Ball.GetSize()/2 && m_Ball.GetPosY() + m_Ball.GetSize()/2 >= m_Bar1.GetPosY() &&  m_Bar1.GetWidth() >= m_Ball.GetPosX())
+            MouseState mouse = Mouse.GetState();
+            switch (CurrentGameState)
             {
-                m_Ball.InverseVelX();
-            }
-            if (m_Bar2.GetPosY() + m_Bar2.GetHeight() >= m_Ball.GetPosY() + m_Ball.GetSize() / 2 && m_Ball.GetPosY() + m_Ball.GetSize() / 2 >= m_Bar2.GetPosY() && graphics.GraphicsDevice.Viewport.Width - m_Bar1.GetWidth() <= m_Ball.GetPosX() + m_Ball.GetSize())
-            {
-                m_Ball.InverseVelX();
+                case Gamestate.MainMenu:
+                    if (PlayButton.IsClicked == true) CurrentGameState = Gamestate.Playing;
+                    PlayButton.Update(mouse);
+                    break;
+                case Gamestate.Playing:
+                    if (Keyboard.GetState().IsKeyDown(Keys.S)) { m_Bar1.SetVel(m_Bar1.GetMaxVel()); }
+                    if (Keyboard.GetState().IsKeyDown(Keys.W)) { m_Bar1.SetVel(-m_Bar1.GetMaxVel()); }
+                    if (Keystate.IsKeyDown(Keys.Down)) { m_Bar2.SetVel(m_Bar2.GetMaxVel()); }
+                    if (Keystate.IsKeyDown(Keys.Up)) { m_Bar2.SetVel(-m_Bar2.GetMaxVel()); }
+                    if (m_Bar1.GetPosY() <= 0 && m_Bar1.GetVel() < 0 || Keyboard.GetState().IsKeyUp(Keys.S) && m_Bar1.GetVel() > 0) { m_Bar1.SetVel(0); }
+                    if (m_Bar1.GetPosY() + m_Bar2.GetHeight() >= graphics.GraphicsDevice.Viewport.Height && m_Bar1.GetVel() > 0 || Keyboard.GetState().IsKeyUp(Keys.W) && m_Bar1.GetVel() < 0) { m_Bar1.SetVel(0); }
+                    if (m_Bar2.GetPosY() <= 0 && m_Bar2.GetVel() < 0 || Keyboard.GetState().IsKeyUp(Keys.Down) && m_Bar2.GetVel() > 0) { m_Bar2.SetVel(0); }
+                    if (m_Bar2.GetPosY() + m_Bar2.GetHeight() >= graphics.GraphicsDevice.Viewport.Height && m_Bar2.GetVel() > 0 || Keyboard.GetState().IsKeyUp(Keys.Up) && m_Bar2.GetVel() < 0) { m_Bar2.SetVel(0); }
+
+
+
+                    if (m_Bar1.GetPosY() + m_Bar1.GetHeight() >= m_Ball.GetPosY() + m_Ball.GetSize() / 2 && m_Ball.GetPosY() + m_Ball.GetSize() / 2 >= m_Bar1.GetPosY() && m_Bar1.GetWidth() >= m_Ball.GetPosX())
+                    {
+                        m_Ball.InverseVelX();
+                    }
+                    if (m_Bar2.GetPosY() + m_Bar2.GetHeight() >= m_Ball.GetPosY() + m_Ball.GetSize() / 2 && m_Ball.GetPosY() + m_Ball.GetSize() / 2 >= m_Bar2.GetPosY() && graphics.GraphicsDevice.Viewport.Width - m_Bar1.GetWidth() <= m_Ball.GetPosX() + m_Ball.GetSize())
+                    {
+                        m_Ball.InverseVelX();
+                    }
+
+                    float MovedPos1 = m_Bar1.GetPosY() + m_Bar1.GetVel() * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    float MovedPos2 = m_Bar2.GetPosY() + m_Bar2.GetVel() * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    float MovedBallPosX = m_Ball.GetPosX() + m_Ball.GetVelX() * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    float MovedBallPosY = m_Ball.GetPosY() + m_Ball.GetVelY() * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                    m_Bar1.SetPos(MovedPos1);
+                    m_Bar2.SetPos(MovedPos2);
+                    m_Ball.SetPosX(MovedBallPosX);
+                    m_Ball.SetPosY(MovedBallPosY);
+
+                    if (m_Ball.GetPosY() + m_Ball.GetSize() >= graphics.GraphicsDevice.Viewport.Height)
+                    {
+                        m_Ball.InverseVelY();
+                    }
+                    else if (m_Ball.GetPosY() < 0 && m_Ball.GetVelY() < 0)
+                    {
+                        m_Ball.InverseVelY();
+                    }
+                    break;
             }
 
-            float MovedPos1 = m_Bar1.GetPosY() + m_Bar1.GetVel() * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            float MovedPos2 = m_Bar2.GetPosY() + m_Bar2.GetVel() * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            float MovedBallPosX = m_Ball.GetPosX() + m_Ball.GetVelX() * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            float MovedBallPosY = m_Ball.GetPosY() + m_Ball.GetVelY() * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            m_Bar1.SetPos(MovedPos1);
-            m_Bar2.SetPos(MovedPos2);
-            m_Ball.SetPosX(MovedBallPosX);
-            m_Ball.SetPosY(MovedBallPosY);
-
-            if (m_Ball.GetPosY() + m_Ball.GetSize() >= graphics.GraphicsDevice.Viewport.Height)
-            {
-                m_Ball.InverseVelY();
-            }
-            else if ( m_Ball.GetPosY() < 0 && m_Ball.GetVelY() < 0)
-            {
-                m_Ball.InverseVelY();
-            }
             base.Update(gameTime);
         }
 
@@ -185,14 +254,23 @@ namespace Pong
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.LightGreen);
             // TODO: Add your drawing code here
             // test
             spriteBatch.Begin();
-            spriteBatch.DrawString(Font, "Test", new Vector2(graphics.GraphicsDevice.Viewport.Width/2, 100), Color.White);
-            spriteBatch.Draw(m_BarShape1, m_Bar1.GetPos(), Color.Red);
-            spriteBatch.Draw(m_BarShape2, m_Bar2.GetPos(), Color.White);
-            spriteBatch.Draw(m_BallShape, m_Ball.GetPos(), Color.White);
+            switch (CurrentGameState)
+            {
+                case Gamestate.MainMenu:
+                    spriteBatch.Draw(Content.Load<Texture2D>("Menu"), new Rectangle(0, 0, graphics.GraphicsDevice.Viewport.Width, graphics.GraphicsDevice.Viewport.Height) , Color.White);
+                    PlayButton.Draw(spriteBatch);   
+                    break;
+                case Gamestate.Playing: 
+                    GraphicsDevice.Clear(Color.LightGreen);
+                    spriteBatch.DrawString(Font, "Test", new Vector2(graphics.GraphicsDevice.Viewport.Width / 2, 100), Color.White);
+                    spriteBatch.Draw(m_BarShape1, m_Bar1.GetPos(), Color.Red);
+                    spriteBatch.Draw(m_BarShape2, m_Bar2.GetPos(), Color.White);
+                    spriteBatch.Draw(m_BallShape, m_Ball.GetPos(), Color.White);
+                    break;
+            }
             spriteBatch.End();
             base.Draw(gameTime);
         }
